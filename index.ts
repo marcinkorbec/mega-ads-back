9899f7ba17fd246ac0df0458c625360484319236
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2022-05-27 14:39:16 +0200

    Added error handling.

--- index.ts
+++ index.ts
@@ -1,12 +1,21 @@
 import express from 'express';
 import cors from 'cors';
 import 'express-async-errors'
+import {handleError} from "./utils/error";
 
 const app = express();
 app.use(cors({
     origin: 'http://localhost:3000'
 }));
 app.use(express.json());
+
+//Routes...
+
+app.get('/', async (req, res) => {
+  throw new Error('Oj no!');
+})
+
+app.use(handleError);
 
 app.listen(3001, '0.0.0.0', () => {
     console.log('Aplikacja nakurwia na porcie http://localhost:3001')
