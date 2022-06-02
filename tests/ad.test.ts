05e1c9a3ee0da2c96f1636f20338c442ca56be44
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2022-06-02 21:41:54 +0200

    Added listing ads.

--- tests/ad.test.ts
+++ tests/ad.test.ts
@@ -1,4 +1,9 @@
 import {AdRecord} from "../records/ad.record";
+import {pool} from "../utils/db";
+
+afterAll(async ()=> {
+  await pool.end();
+})
 
 test('AdRecord returns data from database for one entry.', async ()=> {
   const ad = await AdRecord.getOne('abc');
@@ -11,4 +16,20 @@ test('AdRecord returns data from database for one entry.', async ()=> {
 test('AdRecord returns null from database for unexisting entry.', async ()=> {
   const ad = await AdRecord.getOne('---');
   expect(ad).toBeNull();
+});
+
+test('AdRecord.findAll returns array of found entries.', async ()=> {
+  const ads = await AdRecord.findAll('');
+  expect(ads).not.toEqual([]);
+  expect(ads[0].id).toBeDefined()
+});
+
+test('AdRecord.findAll returns array of found entries when searching for "a".', async ()=> {
+  const ads = await AdRecord.findAll('a');
+  expect(ads[0].id).toBeDefined();
+});
+
+test('AdRecord.findAll returns empty array when searching for something that does not exist.', async ()=> {
+  const ads = await AdRecord.findAll('-----------------------------------------------------');
+  expect(ads).toEqual([]);
 })
